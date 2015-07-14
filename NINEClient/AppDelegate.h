82f2b7b1eda89f5e12855a8c628066935b9a443d
hahaXQC <xmac@sc.10086.com>
2015-07-14 23:13:13 +0800

    Initial Commit

---
+++ NINEClient/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NINEClient
+//
+//  Created by Xmac on 15/7/14.
+//  Copyright (c) 2015年 Drven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NINEClient/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NINEClient
+//
+//  Created by Xmac on 15/7/14.
+//  Copyright (c) 2015年 Drven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
